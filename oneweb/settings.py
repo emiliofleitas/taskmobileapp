35a0beeac6ad4ace2bf21f5f589daf42476d61f3
zhk99 <zohack.skill@gmail.com>
2023-12-25 20:25:23 -0300

    arregle el setting que no dejaba hacer el deploy otra vez

--- oneweb/settings.py
+++ oneweb/settings.py
@@ -87,13 +87,12 @@ DATABASES = {
         default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
 
-    )
+)
+}
                 
 
 #       'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
